9eaa157e511828cd7fc1e9a964033b9c673e0d8f
AlinaMedvedeva <116735202+AlinaMedvedeva@users.noreply.github.com>
2022-10-26 17:15:40 +0300

    Add files via upload

---
+++ Fraction.java
@@ -0,0 +1,109 @@
+package fr;
+
+public class Fraction {
+    private int num;
+    private int denum;
+
+    //конструктор с параметрами, сделанный интелиджей
+    public Fraction(int num, int denum) {
+        this.num = num;
+        if(denum != 0)
+            this.denum = denum;
+        else this.denum = 1;
+    }
+
+    //конструктор ВСЕГДА называется, как класс
+    public Fraction(){
+        num = 1;
+        denum = 1;
+    }
+    //1-ый способ вывода объекта на экран
+    @Override
+    public String toString(){
+        return num + "/" + denum;
+    }
+    //2-ой способ
+    public void print(){
+        System.out.println(num + "/" + denum);
+    }
+
+    public void multyOnKoeff(int koeff){
+        num *= koeff;
+        if(koeff != 0)
+            denum *= koeff;
+    }
+    public Fraction multyOnFrac(Fraction secondf){
+     Fraction result = new Fraction();
+     result.num = num * secondf.num;
+     result.denum = denum*secondf.denum;
+     return result;
+    }
+
+    public Fraction ymn(Fraction sec)
+    {
+        Fraction result = new Fraction();
+        result.num = num*sec.denum;
+        result.denum = denum*sec.num;
+        return result;
+    }
+
+    public Fraction slozh(Fraction sec)
+    {
+        Fraction result = new Fraction();
+        int zn = znam(denum, sec.denum);
+        int k1 = zn/denum, k2 = zn/sec.denum;
+        result.denum = zn;
+        result.num = num*k1 + sec.num*k2;
+        return result;
+    }
+
+    public Fraction vyuch(Fraction sec)
+    {
+        Fraction result = new Fraction();
+        int zn = znam(denum, sec.denum);
+        int k1 = zn/denum, k2 = zn/sec.denum;
+        result.denum = zn;
+        result.num = num*k1 - sec.num*k2;
+        return result;
+    }
+
+    public int znam(int denum, int sd)
+    {
+        int b, m, zn;
+        if(denum > sd)
+        {
+            b = denum;
+            m = sd;
+        }
+        else
+        {
+            b = sd;
+            m = denum;
+        }
+        if(b%m != 0)
+            zn = b*m;
+        else
+            zn = b;
+        return zn;
+    }
+    //геттеры и сеттеры
+
+    //public int getNum() {
+       // return num;
+    //}
+
+    //public void setNum(int num) {
+      //  this.num = num;
+    //}
+
+    //public int getDenum() {
+      //  return denum;
+    //}
+
+    //public void setDenum(int denum) {
+    //    if (denum != 0)
+      //      this.denum = denum;
+    //}
+
+
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+package fr;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String [] args) {
+        Scanner in = new Scanner(System.in);
+        Fraction f1 = new Fraction();
+        int n1 = in.nextInt(), d1 = in.nextInt(), n2 = in.nextInt(), d2 = in.nextInt();
+        Fraction f2 = new Fraction(n1, d1);
+        Fraction f3 = new Fraction(n2, d2);
+        f1 = f2.slozh(f3);
+        System.out.println(f1);
+    }
+}
